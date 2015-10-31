91119df2c516f0c2edc06d9a71bf403c5058ca70
Joe Gandy <Joe@gandy.ws>
2015-10-31 21:30:05 +0000

    Build the logic for finding lobbies, note: this does not include the creation of lobby, just the logic to match players

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ var jade = require('jade');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-function updateGUI(){
-    //io.emit('updateGUI', [Lobbies, Players, Players.length]);
-}
+
+var settings = {
+    min_players:1,
+    max_players:3
+};
 
 function isPlayerInLobby(player_id){
     /*
@@ -32,11 +34,18 @@ function getPlayerLobby(player_id){
     */
 }
 
-var Lobbies = [];
-
+function lobby(type){
+    this.type = type;   //O == private, 1 == public
+    this.open = 1;      //1 == open, 0 == not open
+    this.state = 1;     //0 == ingame, 1 == lobby
+    this.players = [];  //Players/sockets in the lobby
+}
 
 var find_private = 0, find_public = 1, ingame = 2;
 var Players = new Array([],[],[]);
+
+var Public_Lobbies = new Array(new lobby(1), new lobby(1), new lobby(1));
+var Private_Lobbies = new Array(new lobby(2), new lobby(2), new lobby(2));
 
 
 app.get('/', function(req, res){
@@ -81,7 +90,30 @@ io.on('connection', function(socket){
             Players[find_public].push(socket.id);
 
             //Before we find a lobby we need to verify all users are here with a ping request of some kind
+            console.log("Finding an open lobby");
+            var lobby_id = -1;
+            for(var x = Public_Lobbies.length -1; x > -1; x--){
+                if(Public_Lobbies[x].players.length < settings.max_players && Public_Lobbies[x].state == 1){
+                    lobby_id = x;
+                }
+            }
 
+            if(lobby_id == -1){
+                console.log("Could not find a free lobby, please try again later");
+                break;
+            }
+
+            console.log("Moving player to lobby " + lobby_id);
+            Players[find_public].forEach(function(socketId, index){
+                io.to(socketId).emit('join_lobby-' + lobby_id);
+                Public_Lobbies[lobby_id].players.push(socketId);
+                if(Public_Lobbies[lobby_id].players.length >= settings.min_players){
+                    //Start this lobby
+                    Public_Lobbies[lobby_id].open = 0;
+                    console.log("Lobby " + lobby_id + " is now ready to start");
+                }
+            });
+           Players[find_public].length = 0;
             //Find a public lobby here
         break;
         case ingame: //InGame
@@ -95,7 +127,10 @@ io.on('connection', function(socket){
         break;
     }
 
+    console.log("Players:");
     console.log(Players);
+    console.log("Public Lobbies");
+    console.log(Public_Lobbies);
 
     socket.on('disconnect', function(){
         for(var x = 0; x < 3; x++){
