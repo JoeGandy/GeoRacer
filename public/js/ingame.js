3619d182beb7e92c953a1f79645f22ec8053b220
Joe Gandy <joe@gandy.ws>
2016-01-24 20:22:38 +0000

    Better integration of the minimap, added a box to represent borders of the map, created our own icon for handling the player (gives us more control)

--- public/js/ingame.js
+++ public/js/ingame.js
@@ -24,6 +24,21 @@ var map;
 
 var marker_array = new Array();
 
+/*
+	Rome targets:
+	41.898961	12.473089	-	Fiumi Fountain
+	41.8981484	12.473024	-	Pantheon
+
+
+
+
+
+
+
+
+
+*/
+
 function initialize() {
 	//Get our username from previous page
 	var username = localStorage.getItem("username");
@@ -31,7 +46,17 @@ function initialize() {
 	var socket = io.connect(window.location.origin,{query:'page=3&lobby_id='+lobby_id+'&username='+username}); //page=2 means show we're in a lobby
 
 	//Currently a static start location, will be automatic in future
-  	var start_loc = {lat: 40.7206374, lng: -74.000835};
+  	var start_loc = {lat: 41.8978996, lng: 12.4733917};
+  	var bounds = {north: 41.902, south: 41.896, east: 12.480,west: 12.469};
+
+  	var rectangle = new google.maps.Rectangle({
+	    bounds: bounds,
+	    fillColor: "rgba(63, 191, 63, 0.45)",
+	    strokeColor: "rgba(63, 191, 63, 0.73)",
+	    strokeWeight: 2,
+	    clickable: false
+	});
+
 
   	//Let the server know we are ready
   	socket.emit('joined_game', { lobby_id : lobby_id, username : username, loc : start_loc});
@@ -39,8 +64,22 @@ function initialize() {
   	//Setup the minimap
   	map = new google.maps.Map(document.getElementById('mini_map_container'), {
     	center: start_loc,
+      	streetViewControl: false,
     	zoom: 15
   	});
+
+  	var my_marker = new google.maps.Marker({
+		position: start_loc,
+		map: map,
+	    icon: {
+	      	path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+	      	scale: 4,
+	      	rotation: 34+180,
+	      	strokeColor: "rgba(63, 191, 63, 0.73)",
+		    fillColor: "rgba(63, 191, 63, 0.73)",
+		    fillOpacity:1
+	    }
+	});
 
   	//Setup the streetview
   	panorama = new google.maps.StreetViewPanorama(
@@ -51,17 +90,33 @@ function initialize() {
           		pitch: 0
         	}
     });
+
+
+
+	panorama.addListener('pov_changed', function() {
+		my_marker.setIcon({
+		    path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+		    scale: 4,
+		    rotation: panorama.pov.heading+180,
+		    strokeColor: "rgba(63, 191, 63, 0.73)",
+		    fillColor: "rgba(63, 191, 63, 0.73)",
+		    fillOpacity:100
+		});
+	});
+
   	
   	//Let people know when we have changed location
 	panorama.addListener('position_changed', function() {
 		var lat = panorama.getPosition().lat();
 		var lng = panorama.getPosition().lng();
 		map.setCenter({lat: lat, lng: lng});
+		my_marker.setPosition({lat: lat, lng: lng});
   		socket.emit('update_my_position', { username : username, lat : lat, lng : lng, lobby_id : lobby_id});
 	});
 
   	//Bind this panorama to the minimap
   	map.setStreetView(panorama);
+  	rectangle.setMap(map);
 
 	socket.on('update_player', function(result){
 		var players_name = result.username;
@@ -78,7 +133,14 @@ function initialize() {
 			marker_array[marker_array.length] = new google.maps.Marker({
 		      	position: lat_lng,
 		      	map: map,
-		      	icon: 'http://www.fapcia.com/images/map_pin_1.png',
+			    icon: {
+			      	path: google.maps.SymbolPath.BACKWARD_CLOSED_ARROW,
+			      	scale: 4,
+			      	rotation: 0,
+			      	strokeColor: "rgba(25, 25, 25, 0.73)",
+				    fillColor: "rgba(25, 25, 25, 0.73)",
+				    fillOpacity:1
+			    },
 		      	title: players_name
 		  	});
 		} else if (result.length > 0) { //else update the current markers we have
